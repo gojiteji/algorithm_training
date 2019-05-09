2d25d5beb7782510601403bdd5e197105c9c3b79
Koki Tanaka <h27i18@ed.cc.suzuka-ct.ac.jp>
2019-05-09 11:27:25 +0900

    first commit

---
+++ linkedlist.cpp
@@ -0,0 +1,33 @@
+
+''' 
+    ・リストの最後にノードを追加する
+    ・リスト内にノードを挿入する
+    ・リスト内を移動する
+    ・ノードを削除する
+    ・リストを削除する
+    ・参照する
+'''
+
+#include <iostream>
+    using namespace std;
+
+class Node
+{
+public:
+    Node();
+    void append_node();
+    void append_node(unsigned int N);
+    void delete_node();
+    void delete_node(unsigned int N);
+    void refer_node(unsigned int N);
+
+    void 
+
+private:
+};
+
+int main()
+{
+
+    return 0;
+}
